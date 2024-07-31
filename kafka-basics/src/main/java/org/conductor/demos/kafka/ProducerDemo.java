320116bee17d0f79bf9d43775c330c1c71720b47
AASHRITA BILIANGADI <aashrita.biliangadi@gmail.com>
2024-07-31 20:05:51 +0530

    Initial commit

---
+++ kafka-basics/src/main/java/org/conductor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,7 @@
+package org.conductor.demos.kafka;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello!!!");
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "kafka-beginner"
+include("kafka-basics")
