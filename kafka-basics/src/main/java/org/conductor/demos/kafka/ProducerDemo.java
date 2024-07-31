c9f4db89715472f104b04207353b1ff1c1da934b
AASHRITA BILIANGADI <aashrita.biliangadi@gmail.com>
2024-07-31 22:02:31 +0530

    Producer code

--- kafka-basics/src/main/java/org/conductor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/org/conductor/demos/kafka/ProducerDemo.java
@@ -1,5 +1,8 @@
 package org.conductor.demos.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,18 +13,27 @@ public class ProducerDemo {
     private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
 
     public static void main(String[] args) {
-        log.info("------- Welcome -------");
+        log.info("------- I am a Kafka Producer -------");
 
-        //create producer properties
         //connecting insecurely to localhost
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
+        //set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
         //create producer
-        
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //create a producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "msg is hello world");
 
         //send data
+        producer.send(producerRecord);
 
         // flush and close the producer
+        producer.flush(); // tells producer to send all data and block until done
+        producer.close();
     }
 }
