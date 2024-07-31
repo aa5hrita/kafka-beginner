e5aea31939798b3fd3b5b83d49c0ad7ad8de248c
AASHRITA BILIANGADI <aashrita.biliangadi@gmail.com>
2024-07-31 22:53:23 +0530

    Producer code - batch - sticky

--- kafka-basics/src/main/java/org/conductor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/org/conductor/demos/kafka/ProducerDemo.java
@@ -1,7 +1,9 @@
 package org.conductor.demos.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +15,7 @@ public class ProducerDemo {
     private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
 
     public static void main(String[] args) {
-        log.info("------- I am a Kafka Producer -------");
+        log.info("------- I am a Kafka Producer :) -------");
 
         //connecting insecurely to localhost
         Properties properties = new Properties();
@@ -25,13 +27,38 @@ public class ProducerDemo {
 
         //create producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        properties.setProperty("batch.size", "400");
 
         //create a producer record
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "msg is hello world");
+        for (int j = 0; j < 10; j++) { //Batch
 
-        //send data
-        producer.send(producerRecord);
+            for (int i = 0; i < 30; i++) { //Messages in Batch
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "Batch: " + j + " Message: " + i);
 
+                //send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        //executed everytime a record is successfully sent of exception is thrown
+                        if (e == null) {
+                            //the record is successfully sent
+                            log.info("received new metadata: " + "\n" +
+                                    "Topic " + recordMetadata.topic() + "\n" +
+                                    "Partition " + recordMetadata.partition() + "\n" +
+                                    "Offset " + recordMetadata.offset() + "\n" +
+                                    "Timestamp " + recordMetadata.timestamp());
+                        } else {
+                            log.error("error while producing", e);
+                        }
+                    }
+                });
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         // flush and close the producer
         producer.flush(); // tells producer to send all data and block until done
         producer.close();
